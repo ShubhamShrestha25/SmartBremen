2d75b9039376a5f763062e88f602209d9d40d7e2
Shubham Shrestha <shubhamshrestha25@gmail.com>
2026-01-21 21:21:46 +0100

    made help page responsive

--- src/app/help/page.js
+++ src/app/help/page.js
@@ -18,19 +18,21 @@ export default function Help() {
 
         <div className="relative z-20 flex justify-center">
           <div className="mainContainer">
-            <div className="w-1/2 mt-14 mb-6">
+            <div className="mt-14 mb-6 lg:w-1/2">
               <div>
-                <h1 className="text-[#FF4B4B] text-2xl font-medium mb-1.5">
+                <h1 className="text-[#FF4B4B] text-lg font-medium mb-1.5 lg:text-2xl">
                   Moin!
                 </h1>
-                <p className="text-sm">
+                <p className="text-xs lg:text-sm">
                   This page helps you understand how to use the map and explore
                   the city’s informal side.
                 </p>
               </div>
               <div className="mt-4 bg-white/80 p-5 rounded-[20px]">
-                <h1 className="font-medium mb-1">How to Use the Map</h1>
-                <ul className="list-disc text-sm ml-5">
+                <h1 className="font-medium mb-1 text-sm lg:text-base">
+                  How to Use the Map
+                </h1>
+                <ul className="list-disc text-xs ml-5 lg:text-sm">
                   <li>
                     Use the map to explore informal places and everyday
                     activities across Bremen
@@ -42,16 +44,16 @@ export default function Help() {
                     Each place is based on real observations and experiences
                   </li>
                 </ul>
-                <p className="text-sm mt-1">
+                <p className="text-xs mt-1 lg:text-sm">
                   There is no right or wrong way to explore — just follow your
                   curiosity.
                 </p>
               </div>
               <div className="mt-4 bg-white/80 p-5 rounded-[20px]">
-                <h1 className="font-medium mb-1">
+                <h1 className="font-medium mb-1 text-sm lg:text-base">
                   What Is an “Informal” Place?
                 </h1>
-                <ul className="list-disc text-sm ml-5">
+                <ul className="list-disc text-xs ml-5 lg:text-sm">
                   <li>
                     Informal places are locations that people use in their own
                     way, even if they were not officially designed for it.
@@ -69,9 +71,11 @@ export default function Help() {
                 </ul>
               </div>
               <div className="mt-4 bg-white/80 p-5 rounded-[20px]">
-                <h1 className="font-medium mb-1">Who Is This For?</h1>
-                <p className="text-sm">This website is for:</p>
-                <ul className="list-disc text-sm ml-5">
+                <h1 className="font-medium mb-1 text-sm lg:text-base">
+                  Who Is This For?
+                </h1>
+                <p className="text-xs lg:text-sm">This website is for:</p>
+                <ul className="list-disc text-xs ml-5 lg:text-sm">
                   <li>People living in Bremen</li>
                   <li>Newcomers and visitors</li>
                   <li>
@@ -79,9 +83,11 @@ export default function Help() {
                   </li>
                 </ul>
               </div>
-              <div className="flex items-center justify-between mt-4 bg-white/80 h-20 px-5 rounded-[20px]">
-                <h1 className="font-medium">Need More Help?</h1>
-                <button className="w-[152px] h-9 bg-[#6BEE32] rounded-[10px] font-semibold">
+              <div className="flex items-center justify-between mt-4 bg-white/80 h-12 px-5 rounded-[20px] lg:h-20">
+                <h1 className="font-medium text-xs lg:text-base">
+                  Need More Help?
+                </h1>
+                <button className="w-[125px] h-8 bg-[#6BEE32] rounded-[10px] font-semibold text-xs lg:w-[152px] lg:h-6 lg:text-base">
                   CONTACT US
                 </button>
               </div>
